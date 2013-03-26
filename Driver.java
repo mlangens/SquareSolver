18c10e6a7c25487f74fd4a673f750d7555626d86
Max Langensiepen <mlangens@emich.edu>
2013-03-26 14:36:33 -0400

    global format

--- Driver.java
+++ Driver.java
@@ -1,6 +1,7 @@
 public class Driver {
   public static void main(String[] args) {
-    // creates solver object for each positive odd integer 3 to 199 inclusive and prints the output
+    // creates solver object for each positive odd integer 3 to 199 inclusive
+    // and prints the output
     SquareSolver solver;
     for (int i = 3; i <= 199; i += 2) {
       solver = new SquareSolver(i);
