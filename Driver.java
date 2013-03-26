fccd0fac340e0e190c6fb012318daea43af8c16f
Max Langensiepen <mlangens@emich.edu>
2013-03-26 14:48:05 -0400

    label the columns

--- Driver.java
+++ Driver.java
@@ -2,6 +2,7 @@ public class Driver {
   public static void main(String[] args) {
     // creates solver object for each positive odd integer 3 to 199 inclusive
     // and prints the output
+    System.out.println("Input Column       Output Column\n");
     SquareSolver solver;
     for (int i = 3; i <= 199; i += 2) {
       solver = new SquareSolver(i);
